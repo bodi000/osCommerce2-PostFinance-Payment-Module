ed67f5c65431411a4ac302da2322a995658c6023
Bodi Stanciu <bogdanovidiu.stanciu@gmail.com>
2012-12-19 12:42:17 +0100

    changed directory, added and corrected language files

---
+++ includes/languages/french/modules/payment/postfinance.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  $Id: postfinance.php   2009-01-19 16:30
+  Post finance swiss payment gateway - OSCommerce - Payment module : Language file - French.
+*/
+
+  define('MODULE_PAYMENT_POSTFINANCE_TEXT_TITLE', 'PostFinance (Card et E-Finance)');
+  define('MODULE_PAYMENT_POSTFINANCE_TEXT_DESCRIPTION', 'Paiement sécurisé avec PostFinance');
+?>
